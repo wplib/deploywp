019be8d55268d7ca2ef91de38964ac4f4b6b12e5
MickMake <github@mickmake.com>
2020-06-30 20:55:28 +1000

    Tidy up.

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName = "deploywp"
-	BinaryVersion = "1.0.2"
+	BinaryVersion = "1.0.3"
 	SourceRepo = "github.com/wplib/deploywp"
 	BinaryRepo = "github.com/wplib/deploywp"
 )
--- deploywp/tools_workflow.go
+++ deploywp/tools_workflow.go
@@ -9,37 +9,6 @@ import (
 )
 
 
-//func (dwp *TypeDeployWp) test1() {
-//	stateError := ux.NewState("test", false)
-//	stateError.SetError("Hey it failed")
-//
-//	dwp.Print.Intent("test 1 part 1")
-//	dwp.Print.IntentResponse(stateError)
-//	dwp.Print.Intent("test 1 part 2")
-//	dwp.Print.IntentResponse(stateError)
-//}
-//func (dwp *TypeDeployWp) test2() {
-//	stateOk := ux.NewState("test2", false)
-//	stateOk.SetOk("All OK")
-//
-//	dwp.Print.Intent("test 2 part 1")
-//	dwp.Print.IntentResponse(stateOk)
-//	dwp.Print.Intent("test 2 part 2")
-//	dwp.test3()
-//	dwp.Print.IntentResponse(stateOk)
-//	dwp.Print.Intent("test 2 part 3")
-//}
-//func (dwp *TypeDeployWp) test3() {
-//	stateOk := ux.NewState("test3", false)
-//	stateOk.SetOk("All OK")
-//
-//	dwp.Print.Intent( "test 3 part 1")
-//	dwp.Print.IntentResponse(stateOk)
-//	dwp.Print.Intent("test 3 part 2")
-//	dwp.Print.IntentResponse(stateOk)
-//}
-
-
 // This is an alternative to running templates.
 // In theory, the code here, should be able to be replicated in a template file without modification.
 func (dwp *TypeDeployWp) Build() *ux.State {
@@ -50,35 +19,6 @@ func (dwp *TypeDeployWp) Build() *ux.State {
 	for range onlyOnce {
 		dwp.Print.Notify(0, "%s v%s", dwp.Runtime.CmdFile, dwp.Runtime.CmdVersion)
 		dwp.Print.Notify(0, "args: %s", dwp.Runtime.GetArgs())
-
-
-		//stateOk := ux.NewState("error", false)
-		//stateOk.SetOk()
-		//stateError := ux.NewState("ok", false)
-		//stateError.SetError("Hey it failed")
-		//
-		//dwp.Print.Intent( "First thingy - item 1")
-		//dwp.Print.IntentResponse(stateOk)
-		//dwp.Print.Intent( "First thingy - item 2")
-		//dwp.Print.IntentResponse(stateOk)
-		//dwp.Print.Intent( "First thingy - item 3")
-		//dwp.Print.IntentResponse(stateError)
-		//
-		//dwp.test1()
-		//
-		//dwp.Print.Intent( "Second thingy - item 1")
-		//dwp.Print.IntentResponse(stateOk)
-		//dwp.Print.Intent( "Second thingy - item 2")
-		//dwp.Print.Intent( "Second thingy - item 3")
-		//dwp.Print.IntentResponse(stateError)
-		//
-		//dwp.test2()
-		//
-		//dwp.Print.Intent( "Third thingy - item 1")
-		//dwp.Print.IntentResponse(stateOk)
-		//dwp.Print.IntentResponse(stateOk)
-		//dwp.Print.Intent( "Third thingy - item 3")
-		//dwp.Print.IntentResponse(stateError)
 
 
 		{
@@ -122,7 +62,7 @@ func (dwp *TypeDeployWp) Build() *ux.State {
 
 		os.Exit(1)
 
-		// dwp.Print.Intent("Increment BUILD", "Increment BUILD within destination repository")
+		// dwp.Print.Intent("Increment BUILD within destination repository")
 		//dwp.State = dwp.OpenDestinationRepo()
 		time.Sleep(time.Second * 2)	// Simulate
 		if dwp.State.IsError() {
@@ -130,7 +70,7 @@ func (dwp *TypeDeployWp) Build() *ux.State {
 		}
 
 
-		// dwp.Print.Intent("Commit to Pantheon", "Commit destination repository to Pantheon")
+		// dwp.Print.Intent("Commit destination repository to Pantheon")
 		//dwp.State = dwp.OpenDestinationRepo()
 		time.Sleep(time.Second * 2)	// Simulate
 		if dwp.State.IsError() {
@@ -138,7 +78,7 @@ func (dwp *TypeDeployWp) Build() *ux.State {
 		}
 
 
-		// dwp.Print.Intent("Commit source", "Commit source repository")
+		// dwp.Print.Intent("Commit source repository")
 		//dwp.State = dwp.OpenDestinationRepo()
 		time.Sleep(time.Second * 2)	// Simulate
 		if dwp.State.IsError() {
@@ -150,10 +90,71 @@ func (dwp *TypeDeployWp) Build() *ux.State {
 }
 
 
+
+
+//func (dwp *TypeDeployWp) test1() {
+//	stateError := ux.NewState("test", false)
+//	stateError.SetError("Hey it failed")
+//
+//	dwp.Print.Intent("test 1 part 1")
+//	dwp.Print.IntentResponse(stateError)
+//	dwp.Print.Intent("test 1 part 2")
+//	dwp.Print.IntentResponse(stateError)
+//}
+//func (dwp *TypeDeployWp) test2() {
+//	stateOk := ux.NewState("test2", false)
+//	stateOk.SetOk("All OK")
+//
+//	dwp.Print.Intent("test 2 part 1")
+//	dwp.Print.IntentResponse(stateOk)
+//	dwp.Print.Intent("test 2 part 2")
+//	dwp.test3()
+//	dwp.Print.IntentResponse(stateOk)
+//	dwp.Print.Intent("test 2 part 3")
+//}
+//func (dwp *TypeDeployWp) test3() {
+//	stateOk := ux.NewState("test3", false)
+//	stateOk.SetOk("All OK")
+//
+//	dwp.Print.Intent( "test 3 part 1")
+//	dwp.Print.IntentResponse(stateOk)
+//	dwp.Print.Intent("test 3 part 2")
+//	dwp.Print.IntentResponse(stateOk)
+//}
+
+
 func (dwp *TypeDeployWp) TestNewUxState() *ux.State {
 	if state := dwp.IsNil(); state.IsError() {
 		return state
 	}
+
+	//stateOk := ux.NewState("error", false)
+	//stateOk.SetOk()
+	//stateError := ux.NewState("ok", false)
+	//stateError.SetError("Hey it failed")
+	//
+	//dwp.Print.Intent( "First thingy - item 1")
+	//dwp.Print.IntentResponse(stateOk)
+	//dwp.Print.Intent( "First thingy - item 2")
+	//dwp.Print.IntentResponse(stateOk)
+	//dwp.Print.Intent( "First thingy - item 3")
+	//dwp.Print.IntentResponse(stateError)
+	//
+	//dwp.test1()
+	//
+	//dwp.Print.Intent( "Second thingy - item 1")
+	//dwp.Print.IntentResponse(stateOk)
+	//dwp.Print.Intent( "Second thingy - item 2")
+	//dwp.Print.Intent( "Second thingy - item 3")
+	//dwp.Print.IntentResponse(stateError)
+	//
+	//dwp.test2()
+	//
+	//dwp.Print.Intent( "Third thingy - item 1")
+	//dwp.Print.IntentResponse(stateOk)
+	//dwp.Print.IntentResponse(stateOk)
+	//dwp.Print.Intent( "Third thingy - item 3")
+	//dwp.Print.IntentResponse(stateError)
 
 	// Testing ux.State changes.
 	var foo []string
